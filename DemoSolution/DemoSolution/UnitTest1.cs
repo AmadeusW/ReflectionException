0f6d4cdbf96a7c828d4af56c7de45db6dfad863a
Amadeus Wieczorek <git@amadeusw.com>
2014-06-17 13:50:44 -0400

    Installs Roslyn 0.6.4033103

--- DemoSolution/DemoSolution/UnitTest1.cs
+++ DemoSolution/DemoSolution/UnitTest1.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DemoSolution
 {
+    // Install-Package Microsoft.CodeAnalysis -Version 0.6.4033103-beta -Pre
     [TestClass]
     public class UnitTest1
     {
